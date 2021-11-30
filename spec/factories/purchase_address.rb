e7f1d3e016d077f34ab46b367b42663fd6c75c0b
kurigaki <kenji.kurigaki@gmail.com>
2021-11-30 12:56:23 +0900

    単体テストの値の編集

--- spec/factories/purchase_address.rb
+++ spec/factories/purchase_address.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :purchase_address do
     postal_code { '123-4567' }
-    shipping_area_id { 1 }
-    municipalities { '東京都' }
-    address { '1-1' }
-    building_name { '東京ハイツ' }
+    shipping_area_id { Faker::Number.between(from: 1, to: 47) }
+    municipalities { '横浜市緑区' }
+    address { '青山1-1-1' }
+    building_name { '柳ビル103' }
     phone_number { '09012345678' }
   end
 end
